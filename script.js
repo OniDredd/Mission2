7d4c7c343e1a4697f69a79bbe074425b74ff1d84
OniDredd <joshuaf@missionreadyhq.com>
2022-05-25 13:05:21 +1200

    Presentation Final Changes

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function toggleTheme() {
-    // Obtains an array of all <link>
-    // elements.
     // Select your element using indexing.
     let theme = document.getElementsByTagName('link')[0];
-
-    // Change the value of href attribute 
     // to change the css sheet.
     if (theme.getAttribute('href') == 'dark.css') {
         theme.setAttribute('href', 'light.css');
